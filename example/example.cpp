a4598afd6067867e6940a09f784ae941b575f2b6
zhang722 <1755169086@qq.com>
2023-01-17 22:10:09 +0800

    add readme.md

--- example/example.cpp
+++ example/example.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "toml.h"
 
@@ -6,6 +7,10 @@ int main()
 {
     toml::Parser parser("../example.toml");
     auto doc = parser.parse();
-    std::cout << doc.asString() << std::endl;
+    // std::cout << doc.asString() << std::endl;
+
+    auto a = doc.get<toml::Scalar<std::string>>("database.server");
+    std::string b = a->as<std::string>();
+    std::cout << b << std::endl;
     return 0;
 }
